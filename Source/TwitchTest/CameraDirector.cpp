6a2bba4ec21ffdc8f8595e0fea0943c40c932a3a
Paltoquet <thibault.ober@gmail.com>
2016-06-02 10:51:39 +0200

    cameradirector

---
+++ Source/TwitchTest/CameraDirector.cpp
@@ -0,0 +1,29 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TwitchTest.h"
+#include "CameraDirector.h"
+#include "Kismet/GameplayStatics.h"
+
+// Sets default values
+ACameraDirector::ACameraDirector()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACameraDirector::BeginPlay()
+{
+	Super::BeginPlay();
+	APlayerController* OurPlayerController = UGameplayStatics::GetPlayerController(this, 0);
+	OurPlayerController->SetViewTarget(CameraOne);
+}
+
+// Called every frame
+void ACameraDirector::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/TwitchTest/CameraDirector.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "CameraDirector.generated.h"
+
+UCLASS()
+class TWITCHTEST_API ACameraDirector : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+
+	UPROPERTY(EditAnywhere)
+	AActor* CameraOne;
+	// Sets default values for this actor's properties
+	ACameraDirector();
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	
+	// Called every frame
+	virtual void Tick( float DeltaSeconds ) override;
+
+	
+	
+};
