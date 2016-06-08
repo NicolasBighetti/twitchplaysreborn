0f7bd4f2a0a4a413921257541c3d3c63f5176168
Kevin Buisson <buisson.kevin2@gmail.com>
2016-06-08 14:08:21 +0200

    [Fix] All templates functions must be in Header file...

--- Source/TwitchTest/CommandRegistry.h
+++ Source/TwitchTest/CommandRegistry.h
@@ -14,15 +14,64 @@ private:
 	// Commands list
 	TMap<FString, CMD*> Commands;
 public:
-	// FCommandRegistry();
-	~FCommandRegistry();
-
-	// Commands management
-	void Register(CMD* command);
-	CMD* Get(FString name);
-	bool Execute(FString name);
-	bool IsCommand(FString name);
-
 	// World commands
 	static const FCommandRegistry<FWorldCommand> World;
+
+	// === Commands management
+
+	/**
+	  * Registers a command.
+	  */
+	void Register(CMD* command) {
+		// Add command to list
+		Commands.Add(command->GetName(), command);
+	}
+
+	/**
+	  * Gets a command.
+	  */
+	CMD* Get(FString name)
+	{
+		// Check if command exists
+		if (!IsCommand(name)) return NULL;
+
+		// Get command from list
+		return Commands[name];
+	}
+
+	/**
+	  * Executes a command.
+	  */
+	bool Execute(FString name)
+	{
+		CMD* command = Get(name);
+
+		// Execute command
+		if (command)
+		{
+			command->Execute();
+			return true;
+		}
+
+		// Command not found
+		return false;
+	}
+
+	/**
+	  * Checks if it is a command.
+	  */
+	bool IsCommand(FString name)
+	{
+		return Commands.Contains(name);
+	}
+
+	/**
+	  * Free all commands.
+	  */
+	~FCommandRegistry()
+	{
+		// Free registered commands
+		for (auto& cmd : Commands)
+			delete (cmd.Value);
+	}
 };
