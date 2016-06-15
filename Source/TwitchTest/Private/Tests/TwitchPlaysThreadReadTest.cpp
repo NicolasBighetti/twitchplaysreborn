ba663d7d2e3d696cb81feb6047c9c63af77c64f9
Lucas Sauvage <lucas.sauvage.06@gmail.com>
2016-06-15 19:04:35 +0200

    ThreadRead test

---
+++ Source/TwitchTest/Private/Tests/TwitchPlaysThreadReadTest.cpp
@@ -0,0 +1,21 @@
+#include "TwitchPlaysAPI.h"
+#include "AutomationTest.h"
+#include "ThreadRead.h"
+#include "AssertionMacros.h"
+#include "CommandRegistry.h"
+
+IMPLEMENT_SIMPLE_AUTOMATION_TEST(TwitchPlaysThreadReadTest, "TwitchPlays.ThreadReadTest", EAutomationTestFlags::ApplicationContextMask | EAutomationTestFlags::ProductFilter);
+
+// Running test
+bool TwitchPlaysThreadReadTest::RunTest(const FString& Parameters)
+{
+
+	UE_LOG(LogTemp, Display, TEXT("TwitchPlaysThreadReadTest"));
+
+	FCommandRegistry* r = new FCommandRegistry();
+	ThreadRead* t = new ThreadRead(r);
+
+	TWITCH_CHECK(t->Init(), "Error ThreadRead init()");
+	
+    return true;
+}
